8091764d73d3d5abc248c3db9a6b44e902023cec
aselkg <aselekkg@gmail.com>
2020-04-21 18:44:09 -0400

    CRUD function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts # for all routes, we can use this single line or routes down bellow
+  get '/pages', to: 'pages#index'
+  get '/pages/new', to: 'pages#new', as:'new_page'
+  post 'pages', to: 'pages#create'
+  get '/pages/:id', to: 'pages#show', as:'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch '/pages/:id/', to:'pages#update'
+  delete '/pages/:id', to: 'pages#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
