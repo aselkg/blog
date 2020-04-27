b8cae271083350a8979108ad0742c4dd53070cd8
aselkg <aselekkg@gmail.com>
2020-04-27 19:04:01 -0400

    associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts # for all routes, we can use this single line or routes down bellow
+  get 'owners/index'
+
+  get 'owner/index'
+
+   # for all routes, we can use this single line or routes down bellow
+  resources :owners
   get '/pages', to: 'pages#index'
   get '/pages/new', to: 'pages#new', as:'new_page'
   post 'pages', to: 'pages#create'
@@ -7,7 +12,11 @@ Rails.application.routes.draw do
   get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   patch '/pages/:id/', to:'pages#update'
   delete '/pages/:id', to: 'pages#destroy'
-  
+ 
+  #instead of repeating '/posts/:post_id/comments',  can do following instead
+  resources :posts do
+  resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
